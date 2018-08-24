f4083c176036607d72ce7a8b429b2662ea07237a
Christopher Ngai <christopherngai@Christophers-MacBook-Pro-3.local>
2018-08-23 23:19:20 -0700

    finalized css styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,6 +3,9 @@ import React from "react";
 //kelvin-to-fahrenheit npm package
 const kelvinToFahrenheit = require('kelvin-to-fahrenheit');
 
+//uppercase first character
+var ucfirst = require('ucfirst');
+
 const Weather = props => (
 	<div className="weather__info">
 		{ 
@@ -12,17 +15,17 @@ const Weather = props => (
 		}
 		{ 
 		 props.temperature && <p className="weather__key"> Temperature: 
-		 	<span className="weather__value"> { kelvinToFahrenheit(props.temperature) } </span>
+		 	<span className="weather__value"> { kelvinToFahrenheit(props.temperature) } °F</span>
 		 </p>
 		}
 		{ 
 		 props.humidity && <p className="weather__key"> Humidity: 
-		 	<span className="weather__value"> { props.humidity } </span>
+		 	<span className="weather__value"> { props.humidity }% </span>
 		 </p>
 		}
 		{
 		 props.description && <p className="weather__key"> Conditions: 
-		 	<span className="weather__value"> { props.description } </span>
+		 	<span className="weather__value"> { ucfirst(props.description) } </span>
 		 </p>
 		}
 		{
